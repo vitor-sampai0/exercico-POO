fd9699ee5907546a9f65371bb2891dac8b3915e8
vitor sampaio <vitorsampaio.tds2.senai@gmail.com>
2024-08-15 09:29:03 -0300

    exercicio Música

---
+++ musica.js
@@ -0,0 +1,7 @@
+const musica = {
+    nome : "Baby",
+    autor: "Justin bieber",
+    ano: 2010
+};
+
+console.log("A música " + musica.nome + " " +  "foi lançado em " + musica.ano + " pelo " + musica.autor)
