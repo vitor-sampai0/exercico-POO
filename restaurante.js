7e84d635039bb97d5c5b4986021ec7364944b1ba
vitor sampaio <vitorsampaio.tds2.senai@gmail.com>
2024-08-15 09:32:51 -0300

    exercicio Restaurante

---
+++ restaurante.js
@@ -0,0 +1,7 @@
+const restaurante = {
+    nome : "Rasputilio",
+    tipoDeCozinha: "Italiano",
+    localizacao: "Rua domingues"
+};
+
+console.log("O restaurante " + restaurante.nome + " " +  "pode ser encontrado na " + restaurante.localizacao + " e eles fazem comida " + restaurante.tipoDeCozinha)
