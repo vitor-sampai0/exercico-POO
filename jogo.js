2496ef7138509c9dd38f48b1bc1eb0263aeff0e5
vitor sampaio <vitorsampaio.tds2.senai@gmail.com>
2024-08-15 09:36:24 -0300

    exercicio jogo

---
+++ jogo.js
@@ -0,0 +1,7 @@
+const jogo= {
+    nome : "The Last Of US Part 2",
+    Desenvolvedor: "Naughtdog",
+    plataforma: "PS5 e PS4"
+};
+
+console.log("O jogo " + jogo.nome + " " +  "foi desenvolvido  " + jogo.Desenvolvedor + " e pode ser jogado no " + jogo.plataforma)
