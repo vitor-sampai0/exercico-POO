9fbc3e57e9fb06d81cc98dbafc9f3e6b4a866a62
vitor sampaio <vitorsampaio.tds2.senai@gmail.com>
2024-08-15 09:40:22 -0300

    exercicio smartphone

---
+++ restaurante copy.js
@@ -0,0 +1,7 @@
+const celular = {
+    marca : "Iphone",
+    modelo: " Iphone XS",
+    anoDeLancamento: "2018"
+};
+
+console.log("A marca " + celular.marca + " " +  "produziu em " + celular.anoDeLancamento + " o" + celular.modelo)
